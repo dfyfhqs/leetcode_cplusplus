a594764337499e5dff9ddc968fa1a526ff1ad8d2
shijiani <shijiani@ZBMAC-C02D43C0M.local>
2021-03-16 21:22:42 +0800

    add 95

---
+++ 95.不同的二叉搜索树-ii.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=95 lang=cpp
+ *
+ * [95] 不同的二叉搜索树 II
+ *
+ * https://leetcode-cn.com/problems/unique-binary-search-trees-ii/description/
+ *
+ * algorithms
+ * Medium (67.60%)
+ * Likes:    812
+ * Dislikes: 0
+ * Total Accepted:    75.9K
+ * Total Submissions: 112.3K
+ * Testcase Example:  '3'
+ *
+ * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入：3
+ * 输出：
+ * [
+ * [1,null,3,2],
+ * [3,2,null,1],
+ * [3,1,null,null,2],
+ * [2,1,3],
+ * [1,null,2,null,3]
+ * ]
+ * 解释：
+ * 以上的输出对应以下 5 种不同结构的二叉搜索树：
+ * 
+ * ⁠  1         3     3      2      1
+ * ⁠   \       /     /      / \      \
+ * ⁠    3     2     1      1   3      2
+ * ⁠   /     /       \                 \
+ * ⁠  2     1         2                 3
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 <= n <= 8
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        vector<TreeNode*> result;
+        helper(result, 1, n+1);
+        return result;
+    }
+
+    void helper(vector<TreeNode*>& result, int start, int end) {
+        if (start == end) {
+            result.emplace_back(nullptr);
+            return;
+        }
+        for (auto i = start; i < end; ++i) {
+            vector<TreeNode*> left,right;
+            helper(left, start, i);
+            helper(right, i+1, end);
+            for (auto valLeft : left) {
+                for (auto valRight : right) {
+                    TreeNode* newRoot = new TreeNode(i, valLeft, valRight);
+                    result.emplace_back(newRoot);
+                }
+            }
+        }
+    }
+};
+// @lc code=end
+
