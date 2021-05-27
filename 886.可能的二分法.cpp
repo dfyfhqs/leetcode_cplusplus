eb3066315c45f67612890c546241603aef80c4c9
stone <stone>
2021-05-27 22:53:08 +0800

    add

---
+++ 886.可能的二分法.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=886 lang=cpp
+ *
+ * [886] 可能的二分法
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool possibleBipartition(int N, vector<vector<int>>& dislikes) {
+
+    }
+};
+// @lc code=end
+
---
+++ 896.单调数列.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=896 lang=cpp
+ *
+ * [896] 单调数列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isMonotonic(vector<int>& A) {
+
+    }
+};
+// @lc code=end
+
